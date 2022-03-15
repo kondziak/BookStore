862d55c22e5438b2946efec69450bcfaba0e56ee
kondziak <222534@edu.p.lodz.pl>
2022-03-15 22:39:56 +0100

    added showing addresses

--- src/main/java/com/example/application/controller/BillingController.java
+++ src/main/java/com/example/application/controller/BillingController.java
@@ -1,5 +1,6 @@
 package com.example.application.controller;
 
+import com.example.application.model.Billing;
 import com.example.application.model.User;
 import com.example.application.service.BillingService;
 import com.example.application.service.UserService;
@@ -7,9 +8,12 @@ import lombok.NonNull;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/billing")
@@ -25,15 +29,17 @@ public class BillingController {
 
 
     @GetMapping("/get_addresses")
-    public String getAddresses(){
+    public String getAddresses(Model model){
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userService.findUserByEmail(email);
-
-
+        List<Billing> billingList = user.getBillingList();
+        if(billingList.size() != 0){
+            model.addAttribute("billings",billingList);
+        }
         return "get_addresses";
     }
 
-    @PostMapping("/addAddress")
+    @GetMapping("/addAddress")
     public String addAddress(){
 
         return "add_address";
